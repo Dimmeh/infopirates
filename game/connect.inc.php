c9cd701642d60bf9b76582f8c3575e3d49177c81
Fedde <feddevangils@gmail.com>
2014-01-06 13:10:02 +0100

    add infopirates.nl folder

--- game/connect.inc.php
+++ game/connect.inc.php
@@ -1,10 +1,10 @@
 <?php
 
 $host = 'localhost';
-$user = 'root';
-$pass = '1234';
+$user = 'pirate_main';
+$pass = 'SN12pOM6';
 
-$db = 'infopirates';
+$db = 'pirate_main';
 
 $con = mysqli_connect($host, $user, $pass, $db);
 
