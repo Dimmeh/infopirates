878fb9850bf36c2f8c836265308291201ce24b2a
Fedde <feddevangils@gmail.com>
2014-01-06 12:01:45 +0100

    add user_ip in reg_user.php

--- reg_user.php
+++ reg_user.php
@@ -11,12 +11,13 @@ if ($_POST['name'] != '' && $_POST['email'] != '')
 
 	$name =  mysqli_real_escape_string($con, $_POST['name']);
 	$email = mysqli_real_escape_string($con, $_POST['email']);
+	$user_ip = mysqli_real_escape_string($con, $_POST['user_ip']);
 	
 	$_SESSION['name'] = $name;
 
 
 
-	
+
 
 
 
